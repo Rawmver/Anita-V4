fa65dbc91cc288fe0c0c1daf6dc998cd8ca6688f
Rawmver <139946633+Rawmver@users.noreply.github.com>
2025-04-26 11:19:20 +0500

    Update config.js

--- config.js
+++ config.js
@@ -1,17 +1,17 @@
 require('dotenv').config(); 
 
 global.owner = process.env.OWNER_NUMBER; 
-global.anitav4 = process.env.BOT_NAME || "𝐐𝐔𝐄𝐄𝐍_𝐀𝐍𝐈𝐓𝐀-𝐕𝟒";
-global.ownername = process.env.OWNER_NAME || "David Cyril";
-global.packname = process.env.PACK_NAME || "𝐐𝐔𝐄𝐄𝐍_𝐀𝐍𝐈𝐓𝐀-𝐕𝟒";
-global.author = process.env.AUTHOR || "𝐃𝐀𝐕𝐈𝐃 𝐂𝐘𝐑𝐈𝐋";
-global.channelname = process.env.CHANNEL_NAME || "𝐃𝐀𝐕𝐈𝐃 𝐂𝐘𝐑𝐈𝐋";
+global.anitav4 = process.env.BOT_NAME || "MS";
+global.ownername = process.env.OWNER_NAME || "MS";
+global.packname = process.env.PACK_NAME || "LUILOWIEE";
+global.author = process.env.AUTHOR || "TITUUU";
+global.channelname = process.env.CHANNEL_NAME || "MS";
 global.channeljid = process.env.CHANNEL_JID || "120363315231436175@newsletter";
 global.autoTyping = process.env.AUTO_TYPING === 'true'; 
 global.autoRecord = process.env.AUTO_RECORD === 'true';
 global.autoViewStatus = process.env.AUTO_VIEW_STATUS === 'true';
 global.AUTOSTATUS_REACT = process.env.AUTOSTATUS_REACT === 'true';
-global.autoLikeEmoji = process.env.AUTO_LIKE_EMOJI || "💚";
+global.autoLikeEmoji = process.env.AUTO_LIKE_EMOJI || "✔";
 
 global.LEVELUP = process.env.LEVELUP === 'true';
 
